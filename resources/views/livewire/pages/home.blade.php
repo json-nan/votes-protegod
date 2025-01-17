61389c2fe9bf14a139e8a0cd7a661c1c4e7ef5d3
JsonNaN <jasson_alex99@hotmail.com>
2025-01-17 14:22:32 -0600

    hover effect

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -13,7 +13,7 @@ state(['categories' => fn() => Category::with('options')->active()->orderBy('ord
     <div class="grid grid-cols-4 gap-4">
         @foreach ($categories as $category)
             <a href="{{ route('category', $category->id) }}"
-               class="bg-[#1a1224] p-4 rounded-md text-white text-center">{{ $category->name }}</a>
+               class="bg-[#1a1224] p-4 rounded-md text-white text-center hover:bg-[#2d1c2f] transition duration-300 ease-in-out">{{ $category->name }}</a>
         @endforeach
     </div>
 </div>
